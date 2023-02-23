bb3e3b6c521ded8aca7706d6af05159dc63e79a1
2h1z1k <nikolaykalayanov01@gmail.com>
2023-02-23 15:21:39 +0200

    Добавьте файлы проекта.

---
+++ WebApplication1/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+        public DbSet<ApplicationType> ApplicationType { get; set; }
+
+        public DbSet<Product> Product { get; set; }
+    }
+}
